9e1548778a094082d83d9dfa8ef775934f9f665f
dhanvivasani <132878983+dhanvivasani@users.noreply.github.com>
2023-06-08 12:54:08 +0530

    Add files via upload

---
+++ 4083.2.get.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+sum(int a[])
+
+{
+	
+	int i,sum=0,ave,n;
+	printf("enter value :=")
+	scanf("%d",&n);
+	for(i=0;i=<n;i++)
+	{
+		printf("a[%d]=",i);
+		scanf("%d",&a[i]);
+		}	
+		for(i=0;i<=a[i];i++)
+		{
+			sum=sum+a[i];
+		}
+		printf("all element sum:=%d",sum);
+	}
+	main()
+	{
+		int arr[100];
+		sum(arr);
+	}
